f538aa395bd9adb129f6688b8f970a0579b536ee
zionhuang <huangdaoziwen@gamil.com>
2017-10-23 21:57:57 +0800

    MVC初步

---
+++ application/models/Model.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zionhuang
+ * Date: 17-10-22
+ * Time: 下午12:45
+ */
+
+class Model extends Sql
+{
+    protected $_model;
+    protected $_table;
+    public static $dbConfig = [];
+
+    public function __construct()
+    {
+        // 连接数据库
+        $this->connect(self::$dbConfig['host'], self::$dbConfig['username'], self::$dbConfig['password'],
+            self::$dbConfig['dbname']);
+
+        // 获取数据库表名
+        if (!$this->_table) {
+            // 获取模型类名称
+            $this->_model = get_class($this);
+            // 删除类名最后的 Model 字符
+            $this->_model = substr($this->_model, 0, -5);
+
+            // 数据库表名与类名一致
+            $this->_table = strtolower($this->_model);
+        }
+    }
+}
---
+++ config/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zionhuang
+ * Date: 17-10-22
+ * Time: 上午10:34
+ */
+
+//database config
+$config['db']['host'] = 'localhost';
+$config['db']['username'] = 'root';
+$config['db']['password'] = 'huangziwen123';
+$config['db']['dbname'] = 'project';
+
+//default controller and Action
+$config['defaultController'] = 'Item';
+$config['defaultAction'] = 'index';
+
+return $config;
